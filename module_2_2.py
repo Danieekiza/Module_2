4668c31995b80083dceaebd4bd00a12262475965
Danil <koz.love.90@gmail.com>
2024-11-30 15:37:26 +0300

    If elif else

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+# if_elif_else
+first = float(input('Введите первое число: '))
+second = float(input('Введите второе число: '))
+third = float(input('Введите третье число: '))
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(1)
